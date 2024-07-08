72337cf0d8e920086a1b0af792b76c37cb55f2d8
Gopi2923 <dgopidhikshan@gmail.com>
2024-07-08 23:09:43 +0530

    learned filter method.

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const companies= [
   
   const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
   
+  //for loop
 
   for(let i = 0; i < companies.length; i++) {
     console.log(companies[i]);
@@ -29,5 +30,25 @@ const companies= [
 
 
   //Filter 
+//  with loop
+
+//   let canDrink = [];
+//   for(let i = 0; i <= ages.length; i++) {
+//     if(ages[i] >= 21){
+//        canDrink.push(ages[i]);
+//     }
+//   }
+
+//   console.log(canDrink)
+
+//   const canDrink = ages.filter(function(age) {
+//      if(age >= 21) {
+//         return true;
+//      }
+//   })
 
 
+//   console.log(canDrink);
+
+  const canDrink = ages.filter(age => age >= 21);
+  console.log(canDrink)
