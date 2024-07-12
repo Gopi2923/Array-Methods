87ad960710eb53ddffe51a2548bf874ed3260373
Gopi2923 <dgopidhikshan@gmail.com>
2024-07-12 22:33:47 +0530

    Format changes.

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const companies= [
-    {name: "Company One", category: "Finance", start: 1981, end: 2004},
-    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
-    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
-    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
-    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
-    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
-    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
-    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-  ];
-  
-  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
-  
-  //for loop
+const companies = [
+  { name: "Company One", category: "Finance", start: 1981, end: 2004 },
+  { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+  { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+  { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+  { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+  { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+  { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+  { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+  { name: "Company Nine", category: "Retail", start: 1981, end: 1989 }
+];
 
-  for(let i = 0; i < companies.length; i++) {
-    console.log(companies[i]);
-  }
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-  //forEach 
+//for loop
+
+for (let i = 0; i < companies.length; i++) {
+  console.log(companies[i]);
+}
+
+//forEach 
 
 // companies.forEach(company => {
 //     console.log(company.name)
@@ -29,7 +29,7 @@ const companies= [
 // })
 
 
-  //Filter 
+//Filter 
 //  with loop
 
 //   let canDrink = [];
@@ -50,5 +50,5 @@ const companies= [
 
 //   console.log(canDrink);
 
-  const canDrink = ages.filter(age => age >= 21);
-  console.log(canDrink)
+const canDrink = ages.filter(age => age >= 21);
+console.log(canDrink)
